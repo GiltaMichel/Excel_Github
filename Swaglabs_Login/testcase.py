45837c539c0221c258a4331fd383cb634bc03014
Gilta <giltamichel128@gmail.com>
2021-06-29 20:05:18 +0530

    fixing the credentials folder path

--- Swaglabs_Login/testcase.py
+++ Swaglabs_Login/testcase.py
@@ -2,12 +2,10 @@ from selenium import webdriver
 import openpyxl
 import time
 driver = webdriver.Chrome("C:/Users\Gilta\Downloads\chromedriver_win32\chromedriver.exe")
-wb= openpyxl.load_workbook("C:/Users\Gilta\PycharmProjects\Excel\credentials.xlsx")
+wb= openpyxl.load_workbook("C:/Users\Gilta\PycharmProjects\Excel_Github\Swaglabs_Login\credentials.xlsx")
 sheet=wb.active
-#data= sheet['B2'].value
 rows=sheet.max_row
 cols=sheet.max_column
-driver.get("https://www.saucedemo.com/")
 for i in range(2,rows+1):
     driver = webdriver.Chrome("C:/Users\Gilta\Downloads\chromedriver_win32\chromedriver.exe")
     driver.get("https://www.saucedemo.com/")
@@ -20,12 +18,3 @@ for i in range(2,rows+1):
     driver.find_element_by_id("login-button").click()
     time.sleep(5)
     driver.close()
-
-
-
-
-'''
-driver.get("https://www.saucedemo.com/")
-driver.close()
-
-'''
